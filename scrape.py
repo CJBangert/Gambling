2ee0626383e242ac5f8626c976f7d2f526e2449b
cjbangert <cjbangert@DESKTOP-UMHAJ50.localdomain>
2019-10-17 21:25:15 -0400

    scrape live data

--- scrape.py
+++ scrape.py
@@ -3,16 +3,34 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-base_url = "http://www.oddshark.com"
 
-res = requests.get(base_url + "/nba/game-logs")
+def scrape_live_data:
 
-print(res.content)
+	base_url = "https://io.oddsshark.com/upcoming/us2/nba?_=1571350544283"
 
-soup = BeautifulSoup(res.content, "html.parser")
+	headers = {
 
-table = soup.find("div",{"id":"block-system-main"}).find("ul",{"class":"list"}).find_all("li")
+	    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0',
+		'Accept' : 'application/json, text/javascript, */*; q=0.01',
+		'Accept-Language': 'en-US,en;q=0.5',
+		'Accept-Encoding': 'gzip, deflate, br',
+		'Referer': 'https://www.oddsshark.com/nba/game-logs'
+	}
 
-print("Table:")
-print(table) 
+	res = requests.get(base_url, headers = headers)
+
+	f = open("page.html","w+")
+
+	soup = BeautifulSoup(res.content, 'html5lib')
+
+	f.write("%s" % res.content)
+
+	f.close()
+
+# table = soup.find('div', attrs = {'id':'block-system-main'}) 
+
+# #table = soup.find("div",{"id":"block-system-main"}).find("ul",{"class":"list"}).find_all("li")
+
+# print("Table:")
+# print(table) 
 
